b433b095fe746f392867cb75b4058d03d4c6d376
Jake Yamaki <jake.yamaki@gmail.com>
2017-01-25 00:43:23 -0500

    Add basic connection string functionality

--- hqc.py
+++ hqc.py
@@ -10,6 +10,7 @@
 # pip install PyQt4-*.whl
 
 import ConfigParser
+import base64
 import logging
 import sys
 
@@ -19,12 +20,26 @@ from PyQt4.QtGui import QApplication
 
 
 def main():
+    # Parse and load the config
+    # This is for ease of testing and will be replaced with a GUI conn_string input
     confparse = ConfigParser.SafeConfigParser()
     confparse.read('conn.conf')
-    # Currently, config contains username, password, and server address
-    username = confparse.get('ConnectionDetails', 'user')
-    password = confparse.get('ConnectionDetails', 'password')
-    server = confparse.get('ConnectionDetails', 'server')
+    config_username = confparse.get('ConnectionDetails', 'user')
+    config_password = confparse.get('ConnectionDetails', 'password')
+    config_server = confparse.get('ConnectionDetails', 'server')
+
+    # Create the conn_string.
+    # This is for testing only.  Eventually only the producer will be able to do this.
+    # We will also probably have to revise what 'starting a server' looks like.
+    # We will probably have to give the client config files that matches his infrastructure.
+    # We might have this server in the cloud or installed locally, depending on how the client feels.
+    conn_string = make_conn_string(config_username, config_password, config_server)
+
+    # Parse the conn string
+    values = parse_conn_string(conn_string)
+    username = values[0]
+    password = values[1]  # There is an option to pass a pre-hashed password into create_auth_info
+    server = values[2]
 
     logging.basicConfig(level=logging.INFO)
 
@@ -47,19 +62,19 @@ def main():
 
     linphone.set_log_handler(log_handler)
     core = linphone.Core.new(callbacks, None, None)
+    core.video_capture_enabled = False  # remove both of these if we get video implemented
+    core.video_display_enabled = False
     proxy_cfg = core.create_proxy_config()
     proxy_cfg.identity_address = proxy_cfg.normalize_sip_uri("sip:" + username + "@" + server)
     proxy_cfg.server_addr = "sip:" + server
     proxy_cfg.register_enabled = True
 
     auth_info = linphone.Core.create_auth_info(core, username, None, password, None, None, server)
-    # auth_info.username = username
-    # auth_info.password = password
-    # auth_info.domain = server
 
     core.add_proxy_config(proxy_cfg)
     core.add_auth_info(auth_info)
 
+    # Below this is timeout code.  Will sys.exit after some time
     iterate_timer = QTimer()
     iterate_timer.timeout.connect(core.iterate)
     stop_timer = QTimer()
@@ -69,6 +84,24 @@ def main():
 
     exitcode = app.exec_()
     sys.exit(exitcode)
+
+
+def parse_conn_string(conn_string):
+    decoded = base64.b64decode(conn_string)
+    mark1 = decoded.find(';')
+    mark2 = decoded.rfind(';')
+    username = decoded[:mark1]
+    password = decoded[mark1 + 1:mark2]
+    server = decoded[mark2 + 1:]
+    return [username, password, server]
+
+
+def make_conn_string(username, password, server):
+    # For now, conn_string will be sourced from conn.conf
+    # Eventually, it will be passed in through the GUI
+    # If the b64 encoded conn_strings get too long, we can try compressing them
+    conn_string = username + ';' + password + ";" + server
+    return base64.b64encode(conn_string)
 
 
 main()
