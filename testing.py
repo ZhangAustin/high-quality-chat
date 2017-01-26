f862728173cef67710780b3c47c092e6f1fe1202
cptarnie <jonathan.boutsicaris@gmail.com>
2017-01-26 17:21:36 -0500

    added method for splitting an audio file into segments

--- testing.py
+++ testing.py
@@ -1,3 +1,9 @@
 from pydub import AudioSegment
+from pydub.utils import make_chunks
+test_sound = AudioSegment.from_mp3("test.mp3")
 
-sound = AudioSegment.from_mp3("test.mp3")
+chunk_list = make_chunks(test_sound, 10000)
+for i, chunk in enumerate(chunk_list):
+    chunk_name = "test{}".format(i) + ".mp3"
+    print "chunk {}".format(i)
+    chunk.export(chunk_name, format="mp3")
