f2a6e5b85b2cd1b4503ef0d17b877e0118dc7f2a
masterhuddy <hudsonlynam@gatech.edu>
2017-01-26 22:01:53 -0500

    Updated gui.py to reflect.kv file changes

--- gui.py
+++ gui.py
@@ -2,21 +2,23 @@ import kivy
 kivy.require('1.0.7')
 
 from kivy.app import App
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen, FadeTransition
 
 
 class HQC(App):
     def build(self):
-        self.load_kv("HQC.kv")
-        return RootScreen()
+        gui = Builder.load_file("HQC.kv")
+        return gui
 class MainScreen(Screen):
     pass
-class RootScreen(ScreenManager):
+class ScreenManager(ScreenManager):
     pass
 class SessionScreen(Screen):
     pass
 class SessionJoiningScreen(Screen):
-    pass
-
-if __name__ == '__main__':
-    HQC().run()
+    def get_text(self, *args):
+        textinput= self.ids.user_text
+        user = textinput.text
+        print(user)
+HQC().run()
