8db30df5776bccf8569a57c253ffbaa0fd8cff9b
Hudson Lynam <hudsonlynam@gatech.edu>
2017-02-07 21:27:51 -0500

    Changed Artist and Listen Session Joining Screen to accept Connection String, changed Producer to create connection string in conn.conf upon server creation.

--- hqc/gui.py
+++ hqc/gui.py
@@ -4,6 +4,7 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 
 import audio
+import base64
 from hqc import Config
 from hqc import HQCPhone
 
@@ -34,13 +35,39 @@ class SessionScreen(Screen):
         audio.save_audio_chunks(chunk_list, "audio", ".mp3", (os.getcwd() + "/"))
 
 
-class SessionJoiningScreen(Screen):
+class ProducerJoiningScreen(Screen):
     # TODO: Have GUI fill in pre-entered values
     #       Currently a blank field means use existing values, even if none exists
     def gettext(self, servername, username, password):
         config = Config('conn.conf')
         if servername != '':
             config.write('ConnectionDetails', 'server', servername)
+        if username != '':
+            config.write('ConnectionDetails', 'user', username)
+        if password != '':
+            config.write('ConnectionDetails', 'password', password)
+        conn_string = username + ';' + password + ";" + servername
+        encoded = base64.b64encode(conn_string)
+        config.write('ConnectionDetails', 'conn_string', encoded)
+        self.parent.current = 'session'
+
+        phone = HQCPhone(config)
+        phone.add_proxy_config()
+        phone.add_auth_info()
+        phone.make_call(1001, config.get('ConnectionDetails', 'server'))
+class ArtistJoiningScreen(Screen):
+    # TODO: Have GUI fill in pre-entered values
+    #       Currently a blank field means use existing values, even if none exists
+    def gettext(self, constring):
+        config = Config('conn.conf')
+        decoded = base64.b64decode(constring)
+        mark1 = decoded.find(';')
+        mark2 = decoded.rfind(';')
+        username = decoded[:mark1]
+        password = decoded[mark1 + 1:mark2]
+        server = decoded[mark2 + 1:]
+        if server != '':
+            config.write('ConnectionDetails', 'server', server)
         if username != '':
             config.write('ConnectionDetails', 'user', username)
         if password != '':
