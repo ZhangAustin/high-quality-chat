4c3b3fcc1a191367ab58ec8f7a6f74b8c2ab3ebb
Jake Yamaki <jake.yamaki@gmail.com>
2017-03-08 13:42:35 -0500

    Replace the old method with the new

--- hqc/Config.py
+++ hqc/Config.py
@@ -1,12 +1,12 @@
 import ConfigParser
 import logging
 import os
-from logging.config import fileConfig
+
 
 #  Load logging configuration from file
-logging.config.fileConfig('../logging.conf')
+# logging.config.fileConfig('../logging.conf')
 #  Reference logger
-debug_logger = logging.getLogger('debugLog')
+#debug_logger = logging.getLogger('debugLog')
 
 
 class Config(ConfigParser.SafeConfigParser):
@@ -20,39 +20,12 @@ class Config(ConfigParser.SafeConfigParser):
         self.file = file
         if not os.path.isfile(self.file):
             logging.warning("Creating config")
-            #  Open a new file for reading and writing
-            f = open(file, 'w+')
+            # Touch the file
+            f = open(file, 'w')
             f.close()
 
-        self.read(file)
-        if self.has_section('Connection Details'):
-            connection_details = 'Config contains '
-            for item in self.items('ConnectionDetails'):
-                connection_details += "{} ".format(item)
-            debug_logger.debug(connection_details)
-
-        #  Add configuration sections if they do not exist
-        if not self.has_section('ConnectionDetails'):
-            self.add_section('ConnectionDetails')
-
-        if not self.has_section('Settings'):
-            self.add_section('Settings')
-
-        if not self.has_option('ConnectionDetails', 'user'):
-            self.set('ConnectionDetails', 'user', 'None')
-
-        # Risky stuff, best to remove in production
-        if not self.has_option('ConnectionDetails', 'password'):
-            self.set('ConnectionDetails', 'password', 'None')
-
-        if not self.has_option('ConnectionDetails', 'server'):
-            self.set('ConnectionDetails', 'server', 'None')
-
-        if not self.has_option('Settings', 'mic'):
-            self.set('Settings', 'mic', 'None')
-
-        if not self.has_option('Settings', 'speakers'):
-            self.set('Settings', 'speakers', 'None')
+        self.read(self.file)
+        self.check()
 
         with open(self.file, 'w') as config_file:
             self.write(config_file)
@@ -91,5 +64,5 @@ class Config(ConfigParser.SafeConfigParser):
         with open(self.file, "w") as config_file:
             self.write(config_file)
 
-#  Initialize Config on import. Reference this config for usage
-config = Config("conn.conf")
+# WARNING: DO NOT Initialize Config on import. Create a config object with custom parameters.
+#config = Config("conn.conf")
